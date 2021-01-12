bfe98d7bbf04d6887f7d0933131ac50d107529bd
Alejandro <a.chataing.a@gmail.com>
2021-01-11 20:42:48 -0400

    New commit

--- public/class-beautiful-elementor-timeline-widget-public.php
+++ public/class-beautiful-elementor-timeline-widget-public.php
@@ -73,7 +73,7 @@ class Beautiful_Elementor_Timeline_Widget_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/beautiful-elementor-timeline-widget-public.scss', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/beautiful-elementor-timeline-widget-public.css', array(), $this->version, 'all' );
 
 	}
 
