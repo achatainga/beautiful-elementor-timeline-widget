69c979b2b6e7518da6ee4377f36478ee3d12be90
Alejandro <a.chataing.a@gmail.com>
2021-01-11 21:29:00 -0400

    New commit

--- includes/class-beautiful-elementor-timeline-widget.php
+++ includes/class-beautiful-elementor-timeline-widget.php
@@ -170,7 +170,7 @@ class Beautiful_Elementor_Timeline_Widget {
 
 		$plugin_public = new Beautiful_Elementor_Timeline_Widget_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles', 100 );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
